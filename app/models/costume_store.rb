a41a0b18cccaec8497ad0d397abd10517420badc
Ivana <ivana_pile@yahoo.com>
2020-01-03 19:07:02 -0500

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,13 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.string :price
+            t.string :image_url
+            t.integer :size
+            t.timestamps
+        end
+      end
+end
